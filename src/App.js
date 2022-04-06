6669232b96de52cdd622fef8b075e9bc96fc6450
kimseowoo03 <kimseowoo03@gmail.com>
2022-04-07 01:11:18 +0900

    리액트 CRUD 구현 To Do List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from 'react';
 
-function App() {
+function App() { 
+  const [liValue, setLiValue] = useState(''); //값 바꾸기
+  const [lis, setLis] = useState([]); // li 객체들
+  // 이미 만들어 놨네요 이거 수정에 쓰면 됨
+  const [changeValue, setChangeValue] = useState('');
+
+
+  const changeFlag = (id) => {
+   setLis(lis.map(li => {
+     if(li.id === id) {
+       li.flag = true;
+     }; // 내가 선택한 거 true로 바꾸고
+     return li // li들 출력
+   }));
+  }
+
+  const addList = () => {
+    setLis([
+      ...lis,
+      {value: liValue, flag: false, id: Date.now() },
+    ]);
+    setLiValue('');
+  }
+
+  const deletBtn = (targetId) => {
+    setLis(lis.filter(li => {
+      return li.id !== targetId ;
+    }));
+  };
+
+  const edit = (value, id) => {
+    setLis(lis.map(li => {
+      if(li.id === id) {
+        console.log(value, id)
+      }
+      return li
+    }));
+  };
+  
+  const renderList = lis.map(li => {
+    return(
+      <li key={li.id}>
+        {li.flag ?( // flag = true
+        <input type='text' defaultValue={li.value} onChange={(e) => setChangeValue(e.target.value)}/>
+        ) :( // flag = flase
+        <>{li.value}</>
+        )}
+        {li.flag ?(
+          <>
+          <button onClick={() => deletBtn(li.id)}>삭제</button>
+          <button onClick={() => edit(changeValue, li.id)}>확인</button>
+          </>
+        ) :(
+          <>
+          <button onClick={() => deletBtn(li.id)}>삭제</button>
+          <button onClick={() => changeFlag(li.id)}>수정</button>
+          </>
+        )}
+      </li>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To Do List</h1>
+      <div>
+        <input type='text' value={liValue} onChange={(e) => setLiValue(e.target.value)} />
+        <button onClick={addList}>+</button>
+        {renderList}
+      </div>
+      <ul>
+
+      </ul>
     </div>
   );
 }
-
 export default App;
+
